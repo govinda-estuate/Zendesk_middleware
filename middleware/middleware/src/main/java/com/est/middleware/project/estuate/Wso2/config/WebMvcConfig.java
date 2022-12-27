5c2183e46800eb30dd9e5097287079353da19c04
Govinda <govinda>
2022-12-27 12:42:58 +0530

    first commit

---
+++ middleware/middleware/src/main/java/com/est/middleware/project/estuate/Wso2/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.est.middleware.project.estuate.Wso2.config;
+
+ 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+  @Bean
+  public ViewResolver viewResolver() {
+    InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+    resolver.setPrefix("/WEB-INF/jsp/");
+    resolver.setSuffix(".jsp");
+    return resolver;
+  }
+
+}
+
